3b9355f2538e3e2571d3a6bb914f8918136f5708
IanaAntonova <155672267+IanaAntonova@users.noreply.github.com>
2024-09-12 20:32:53 -0400

    Example_get.py added.

---
+++ Example_get.py
@@ -0,0 +1,14 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+service = ChromeService(ChromeDriverManager().install())
+browser = webdriver.Chrome(service=service)
+
+browser.get("https://www.google.com")
+time.sleep(3)
+browser.refresh()
+time.sleep(3)
+browser.back()
+browser.forward()
